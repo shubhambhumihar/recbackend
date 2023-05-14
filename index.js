c88a44fb7f063a08cd5acf9031c83c61873484c2
Shubham <sk4957199@gmail.com>
2023-05-14 13:12:54 +0530

    fixed route

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ dotenv.config();
 mongoose.set("strictQuery", false);
 databaseConnect();
 
+app.get("/").send("Server is working");
+
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
